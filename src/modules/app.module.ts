933fc39306446a845dd96e44331bfab69e919f8a
Clément <clementfiesta@gmail.com>
2025-06-21 09:35:35 +0200

    Remove GamesModule & SavesModule

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,9 @@
 import { AuthModule } from '@auth/auth.module';
 import { ConfigModule } from '@modules/config/config.module';
-import { GamesModule } from '@modules/games/games.module';
-import { SavesModule } from '@modules/saves/saves.module';
 import { Module } from '@nestjs/common';
 import { UsersModule } from '@users/users.module';
 
 @Module({
-  imports: [ConfigModule, UsersModule, AuthModule, GamesModule, SavesModule],
+  imports: [ConfigModule, UsersModule, AuthModule],
 })
 export class AppModule {}
